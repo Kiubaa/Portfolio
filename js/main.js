aea77c6d163fb6ef88d665ae6bbf4088b8fc2396
desarrollo <desarrollo@dronearth.co>
2024-04-17 16:42:41 -0500

    Portfolio V1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,52 @@
 window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  function myFunction() {
+    if (window.scrollY >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+}
+
+let slider = document.querySelector(".slider");
+let li = document.querySelectorAll(".container .navbar ul li");
+
+let index_value = 0;
+let left_position = 0;
+
+li.forEach((item, index) => {
+  slider.style.width = li[0].clientWidth + "px";
+  slider.style.left = left_position + "px";
+
+  item.onclick = function (){
+    slider.style.width = item.clientWidth + "px";
+    // console.log(index);
+    index_value = index;
+    get_left_position();
+    slider.style.left = left_position + "px";
+    left_position = 0;
+  }
+});
+
+function get_left_position(){
+  for (let i = 0; i < index_value; i++) {
+    const element = li[i];
+    left_position += element.clientWidth;
+    console.log(left_position);
   }
 }
+
+var menu = document.getElementById('navbar');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add('active');
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove('active');
+});
